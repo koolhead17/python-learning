0564c92cab511184b1b4413022a5e578d3de8033
koolhead17 <koolhead17@gmail.com>
2018-11-11 17:40:44 +0530

    added more exercises

---
+++ relational-operator.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age:"))
+
+if age >= 25:
+    print("Please collect your card. You are eligible for it.")
+
+else:
+    print("You have to wait till you are 25 years old for the card")
---
+++ while.py
@@ -0,0 +1,12 @@
+print("Playing with while loop")
+print("###### starting while loop #######")
+
+num = 0
+while num < 3:
+    print("The value of the number is: " + str(num))
+    print("The number is inside while loop")
+
+    num = num + 1
+
+print(" ######## ending While loop ##########")
+print("The loop gets over as num > 3")    
