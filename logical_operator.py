6f03c2aa8a4260b62eec040c1a49920cde006847
koolhead17 <koolhead17@gmail.com>
2018-11-15 14:59:45 +0530

    more exercises

---
+++ logical_operator.py
@@ -0,0 +1,23 @@
+print(not(True))
+print(not(False))
+
+print()
+
+print(True and False)
+print(True and True)
+print(False and True)
+print(False and False)
+
+print()
+
+print(False or False)
+print(False or True)
+print(True or False)
+print(True or True)
+
+print()
+
+a =True
+b =False
+c = (a or b)
+print(c)
---
+++ logical_operator_ex.py
@@ -0,0 +1,6 @@
+age = int(input("enter your age: "))
+income = int(input("Enter your yearly income: "))
+if (age >= 18) and (income >= 15000):
+    print("Congratulations, you are eligible for loan")
+else:
+    print("Sorry, you are not eligilble for loan")    
