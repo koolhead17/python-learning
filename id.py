8cc2653db905b1219de5e2f77165a2b98f902550
koolhead17 <koolhead17@gmail.com>
2018-11-20 17:06:56 +0530

    more exercises

---
+++ id.py
@@ -0,0 +1,7 @@
+a = 5
+print('Value of variable "a" is -> ', a,', and its ID of a is ->', id(a),sep=' ')
+print()
+a = 6
+print('Value of variable "a" is -> ', a,', and its new ID is ->', id(a),sep=' ')
+
+
---
+++ type.py
@@ -0,0 +1,25 @@
+a = 6
+b = 6.0
+c = [1,2,3,4,5]
+d=(1,2,3,4,5)
+e = {1,2,3,4,5}
+f= 'star trek'
+
+print('Type of', 'a is',type(a),sep=' ')
+print()
+print('Type of', 'b is',type(b),sep=' ')
+print()
+print('Type of', 'c is',type(c),sep=' ')
+print()
+print('Type of', 'd is',type(d),sep=' ')
+print()
+print('Type of', 'e is',type(e),sep=' ')
+print()
+print('Type of', 'f is',type(f),sep=' ')
+
+
+
+
+
+
+
