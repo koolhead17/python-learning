8f9af6d75eb4c7966a8e264c654f8ae8538a7fcb
koolhead17 <koolhead17@gmail.com>
2018-12-02 11:11:02 +0530

    more examples

---
+++ string_slicing.py
@@ -0,0 +1,24 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+alphabet_slice1 = alphabet[:20:1]
+print(alphabet_slice1)
+
+alphabet_slice2 = alphabet[3:20:1]
+print(alphabet_slice2)
+
+alphabet_slice3 = alphabet[::]
+print(alphabet_slice3)
+
+alphabet_slice4 = alphabet[2:20:2]
+print(alphabet_slice4)
+
+alphabet_slice5 = alphabet[:20:2]
+print(alphabet_slice5)
+
+alphabet_slice6 = alphabet[2::2]
+print(alphabet_slice6)
+
+
+alphabet_slice7 = alphabet[::4]
+print(alphabet_slice7)
+
+
