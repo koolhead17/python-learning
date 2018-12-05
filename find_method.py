2e81e0f554c065d27e4d8b168085cc1c5337bf7b
koolhead17 <koolhead17@gmail.com>
2018-12-05 08:17:43 +0530

    more examples

---
+++ find_method.py
@@ -0,0 +1,12 @@
+str_1 = 'never go out with stranges.'
+index_position = str_1.find('with',)
+print(index_position)
+
+index_position = str_1.find('with',7,-1)
+print(index_position)
+
+index_position = str_1.find('with',-3,-1)
+print(index_position)
+
+index_position = str_1.find('with',12,18)
+print(index_position)
---
+++ url_string.py
@@ -0,0 +1,5 @@
+html_string = '<a href="http://atuljha.com">'
+print(html_string)
+url = html_string[9:-2:1]
+print(url)
+
