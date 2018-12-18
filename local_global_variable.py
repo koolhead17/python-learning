c141bf3e595b102894f93087961e9458e061fedc
koolhead17 <koolhead17@gmail.com>
2018-12-18 07:57:19 +0530

    more examples

---
+++ local_global_variable.py
@@ -0,0 +1,12 @@
+cat_name = "ayah"
+def cat_names(cat_name1, cat_name2):
+     cat_name1 = "jugnu"
+     cat_name2 = " bingu"
+     cat_name = cat_name1 + cat_name2
+     print(cat_name)
+     print(cat_name1)
+
+
+print(cat_name)
+print()
+cat_names("fu", "bar")
